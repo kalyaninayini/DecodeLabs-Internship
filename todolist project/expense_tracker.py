a6ecd01bd81b7ebc0f882e0be5e102b44ffd8b0a
Kalyani Nayini <kalyaninayini2006@gmail.com>
2026-05-30 13:56:39 +0530

    Added Task 2 Expense_Tracker

---
+++ todolist project/expense_tracker.py
@@ -0,0 +1,14 @@
+# Expense Tracker Program
+
+total = 0
+
+expense1 = int(input("Enter first expense: "))
+total = total + expense1
+
+expense2 = int(input("Enter second expense: "))
+total = total + expense2
+
+expense3 = int(input("Enter third expense: "))
+total = total + expense3
+
+print("Total Spent:", total)
